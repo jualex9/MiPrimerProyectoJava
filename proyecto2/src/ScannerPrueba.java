2ef7adab655568716d9f800b76864d51009e9c75
“jualex9” <jualexmanzur@gmail.com>
2025-03-02 16:24:51 -0500

    uso del Scanner

---
+++ proyecto2/src/ScannerPrueba.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ScannerPrueba{
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = Scanner.nextLine();
+        System.out.print("Ingrese su edad: ");
+        int edad = Scanner.nextByte();
+        Scanner.close();
+
+        System.out.println("Hola, " + nombre + ". Tienes " + edad + " años.");
+    }
+}
