f8a93b6a1436ee304477c34f3cea345923a5b6f1
jualex9 <jualex828@gmail.com>
2025-03-14 07:52:00 -0500

    ejercicio resuelto quiz

---
+++ src/ejercicioQuiz.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ejercicioQuiz
+{
+   public static void main(String[] args)
+   {
+    Scanner input =new Scanner(System.in);
+         System.out.println("ingrese su numero");
+       int numero = input.nextInt();
+       System.out.println("el numero" + numero);
+       if (numero >0) {
+        for ( int k=1; k<=numero; k++) {
+           for (int i=1; i<=k; i++) {
+            if ( i% 2 == 1) {                
+                System.out.print(k);
+            } else {
+                System.out.print(k*-1);
+            }
+        if(i<k) {
+            System.out.print(" ");
+            
+        }
+ }
+        System.out.println();
+        }
+        input.close();
+    } else {
+        System.out.println("numero no valido");
+        input.close();
+       
+        return;
+       
+
+    }    
+}
+       }
+    
+
+   
+
+
+
+
