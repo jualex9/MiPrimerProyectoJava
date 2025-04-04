fc0ef521602840967ff065036b8be5ac7ff7117b
jualex9 <jualex828@gmail.com>
2025-04-04 07:05:08 -0500

    ejercicios

---
+++ src/Matriz1.java
@@ -0,0 +1,19 @@
+public class Matriz1 {
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        for (int i = 0; i < matriz.length; i++) { // Recorre las filas
+            for (int j = 0; j < matriz[i].length; j++) { // Recorre las columnas
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(); // Salto de línea después de cada fila
+        }
+    }
+}
+
+
---
+++ src/Matriz2.java
@@ -0,0 +1,17 @@
+public class Matriz2 {
+    public static void main(String[] args) {
+        char[][] tablero = {
+            {'X', 'O', 'X'},
+            {'O', 'X', ' '},
+            {' ', 'O', ' '}
+        };
+        
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
